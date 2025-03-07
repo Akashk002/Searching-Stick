a87d994f1c2934e7e7813020973dd06fd82061ee
Akash <akash.katailiha120@gmail.com>
2025-03-07 20:01:27 +0530

    Implement Binary Search and its Time Complexity

--- Searching-Stick/source/Gameplay/StickCollection/StickCollectionController.cpp
+++ Searching-Stick/source/Gameplay/StickCollection/StickCollectionController.cpp
@@ -6,7 +6,6 @@
 #include "Global/ServiceLocator.h"
 #include "Sound/SoundService.h"
 #include <random>
-#include <iostream>
 
 namespace Gameplay {
 	namespace Collection {
@@ -61,10 +60,113 @@ namespace Gameplay {
 			}
 		}
 
+		void Gameplay::Collection::StickCollectionContoller::shuffleSticks()
+		{
+			std::random_device device;
+			std::mt19937 random_engine(device());
+
+			std::shuffle(sticks.begin(), sticks.end(), random_engine);
+		}
+
+		void StickCollectionContoller::sortElements()
+		{
+			std::sort(sticks.begin(), sticks.end(), [this](const Stick* a, const Stick* b) { return compareElementsByData(a, b); });
+			updateSticksPosition();
+		}
+
+		bool StickCollectionContoller::compareElementsByData(const Stick* a, const Stick* b) const
+		{
+			return a->data < b->data;
+		}
+
 		void Gameplay::Collection::StickCollectionContoller::resetSticksColor()
 		{
 			for (int i = 0; i < sticks.size(); i++)
 				sticks[i]->stick_view->setFillColor(collection_model->element_color);
+		}
+
+		void Gameplay::Collection::StickCollectionContoller::resetVariables()
+		{
+			number_of_comparisons = 0;
+			number_of_array_access = 0;
+		}
+
+		void Gameplay::Collection::StickCollectionContoller::resetSearchStick()
+		{
+			stick_to_search = sticks[std::rand() % sticks.size()];
+			stick_to_search->stick_view->setFillColor(collection_model->search_element_color);
+		}
+
+		void Gameplay::Collection::StickCollectionContoller::processSearchThreadState()
+		{
+			if (search_thread.joinable() && stick_to_search == nullptr)
+			{
+				joinThreads();
+			}
+		}
+
+		void Gameplay::Collection::StickCollectionContoller::joinThreads()
+		{
+			search_thread.join();
+		}
+
+		void Gameplay::Collection::StickCollectionContoller::processLinearSearch()
+		{
+			Sound::SoundService* sound_service = Global::ServiceLocator::getInstance()->getSoundService();
+			for (int i = 0; i < sticks.size(); i++)
+			{
+
+
+				number_of_array_access += 1;
+				number_of_comparisons++;
+
+				sound_service->playSound(Sound::SoundType::COMPARE_SFX);
+
+				if (sticks[i] == stick_to_search)
+				{
+					stick_to_search->stick_view->setFillColor(collection_model->found_element_color);
+					stick_to_search = nullptr;
+					return;
+				}
+				else
+				{
+					sticks[i]->stick_view->setFillColor(collection_model->processing_element_color);
+					std::this_thread::sleep_for(std::chrono::milliseconds(current_operation_delay));
+					sticks[i]->stick_view->setFillColor(collection_model->element_color);
+				}
+
+			}
+		}
+
+		void StickCollectionContoller::processBinarySearch()
+		{
+			int left = 0;
+			int right = sticks.size();
+
+			Sound::SoundService* sound_service = Global::ServiceLocator::getInstance()->getSoundService();
+			while (left < right)
+			{
+				int mid = left + (right - left) / 2;
+				number_of_array_access += 2;
+				number_of_comparisons++;
+
+				sound_service->playSound(Sound::SoundType::COMPARE_SFX);
+
+				if (sticks[mid] == stick_to_search)
+				{
+					sticks[mid]->stick_view->setFillColor(collection_model->found_element_color);
+					stick_to_search = nullptr;
+					return;
+				}
+
+				sticks[mid]->stick_view->setFillColor(collection_model->processing_element_color);
+				std::this_thread::sleep_for(std::chrono::milliseconds(current_operation_delay));
+				sticks[mid]->stick_view->setFillColor(collection_model->element_color);
+
+				number_of_array_access++;
+				if (sticks[mid]->data <= stick_to_search->data) left = mid;
+				else right = mid;
+			}
 		}
 
 		void Gameplay::Collection::StickCollectionContoller::initializeSticksArray()
@@ -104,12 +206,19 @@ namespace Gameplay {
 		void Gameplay::Collection::StickCollectionContoller::initialize()
 		{
 
+			collection_model->initialize();
 			initializeSticks();
+
 			reset();
+
+			time_complexity = "XYZ";
 		}
 
 		void Gameplay::Collection::StickCollectionContoller::update()
 		{
+			processSearchThreadState();
+
+			collection_view->update();
 
 			for (int i = 0; i < sticks.size(); i++)
 				sticks[i]->stick_view->update();
@@ -117,6 +226,7 @@ namespace Gameplay {
 
 		void Gameplay::Collection::StickCollectionContoller::render()
 		{
+			collection_view->render();
 			for (int i = 0; i < sticks.size(); i++)
 				sticks[i]->stick_view->render();
 		}
@@ -124,6 +234,9 @@ namespace Gameplay {
 		void Gameplay::Collection::StickCollectionContoller::reset()
 		{
 			current_operation_delay = 0;
+
+			if (search_thread.joinable()) search_thread.join();
+
 			shuffleSticks();
 			updateSticksPosition();
 			resetSticksColor();
@@ -139,8 +252,14 @@ namespace Gameplay {
 			{
 			case Gameplay::Collection::SearchType::LINEAR_SEARCH:
 				time_complexity = "O(n)";
-				current_operation_delay = collection_model->linear_search_delay;;
+				current_operation_delay = collection_model->linear_search_delay;
 				search_thread = std::thread(&StickCollectionContoller::processLinearSearch, this);
+				break;
+			case Gameplay::Collection::SearchType::BINARY_SEARCH:
+				sortElements();
+				time_complexity = "O(log n)";
+				current_operation_delay = collection_model->binary_search_delay;
+				search_thread = std::thread(&StickCollectionContoller::processBinarySearch, this);
 				break;
 			}
 		}
@@ -148,11 +267,6 @@ namespace Gameplay {
 		SearchType Gameplay::Collection::StickCollectionContoller::getSearchType()
 		{
 			return search_type;
-		}
-
-		int Gameplay::Collection::StickCollectionContoller::getNumberOfSticks()
-		{
-			return collection_model->number_of_elements;
 		}
 
 		int Gameplay::Collection::StickCollectionContoller::getNumberOfComparisons()
@@ -165,75 +279,19 @@ namespace Gameplay {
 			return number_of_array_access;
 		}
 
+		int Gameplay::Collection::StickCollectionContoller::getNumberOfSticks()
+		{
+			return collection_model->number_of_elements;
+		}
+
 		int Gameplay::Collection::StickCollectionContoller::getDelayMilliseconds()
 		{
 			return current_operation_delay;
 		}
 
-		std::string Gameplay::Collection::StickCollectionContoller::getTimeComplexity()
+		sf::String Gameplay::Collection::StickCollectionContoller::getTimeComplexity()
 		{
 			return time_complexity;
-		}
-
-		void Gameplay::Collection::StickCollectionContoller::shuffleSticks()
-		{
-			std::random_device device;
-			std::mt19937 random_engine(device());
-
-			std::shuffle(sticks.begin(), sticks.end(), random_engine);
-		}
-
-		void Gameplay::Collection::StickCollectionContoller::resetSearchStick()
-		{
-			stick_to_search = sticks[std::rand() % sticks.size()];
-			stick_to_search->stick_view->setFillColor(collection_model->search_element_color);
-		}
-
-
-		void Gameplay::Collection::StickCollectionContoller::resetVariables()
-		{
-			number_of_array_access = 0;
-			number_of_comparisons = 0;
-		}
-
-		void Gameplay::Collection::StickCollectionContoller::processSearchThreadState()
-		{
-			if (search_thread.joinable() && stick_to_search == nullptr)
-			{
-				joinThreads();
-			}
-		}
-		void Gameplay::Collection::StickCollectionContoller::joinThreads()
-		{
-			search_thread.join();
-		}
-
-		void Gameplay::Collection::StickCollectionContoller::processLinearSearch()
-		{
-			Sound::SoundService* sound_service = Global::ServiceLocator::getInstance()->getSoundService();
-
-			for (int i = 0; i < sticks.size(); i++)
-			{
-
-				number_of_array_access += 1;
-				number_of_comparisons++;
-
-				sound_service->playSound(Sound::SoundType::COMPARE_SFX);
-
-				if (sticks[i] == stick_to_search)
-				{
-					stick_to_search->stick_view->setFillColor(collection_model->found_element_color);
-					stick_to_search = nullptr;
-					return;
-				}
-				else
-				{
-					sticks[i]->stick_view->setFillColor(collection_model->processing_element_color);
-					std::this_thread::sleep_for(std::chrono::milliseconds(current_operation_delay));
-					sticks[i]->stick_view->setFillColor(collection_model->element_color);
-				}
-
-			}
 		}
 
 	}
